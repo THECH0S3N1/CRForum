26ef84870175a4857f373bef7de8f89b33764df5
THECH0S3N1 <40071792+THECH0S3N1@users.noreply.github.com>
2018-10-07 19:13:13 +0200

    blockchain alpha version 1

--- CRForum/BlockBase.swift
+++ CRForum/BlockBase.swift
@@ -11,32 +11,18 @@ import UIKit
 import Firebase
 import CoreData
 
+//chars to choose the hash elements
 var hashBase = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMOPQRSTUVWXYZ0123456789"
 
 //data to be used from the blockchainData.xcdatamodeld
-
-
+//segment describing the blocks of the blockchain, including the hash function for next blocks.
+//for simplicity, only 16 character hashes will be used.
 
 class Blocks{
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var hash = String()
     var prevHash = String()
     var index = Int()
-    //let data = NSFetchRequest<NSFetchRequestResult>(entityName: "UserData")
-    
-    /*func fetchData()-> NSManagedObject {
-        let data = NSFetchRequest<NSFetchRequestResult>(entityName: "UserData")
-        data.fetchLimit = 1
-        let user = try! context.fetch(data)
-        return user
-     
-        
-    }*/
-    
-
-
-
-
+    var userData = String()
     func generateNewHash()->String{
         var hash = ""
         for _ in 0..<16{
@@ -45,24 +31,49 @@ class Blocks{
         }
         return hash
     }
-    
-    
-
 }
+
+//blockchain class build-up of the blocks
 
 class BlockChain{
     
     var blockChain = [Blocks]()
-    
-    
-    func createMerkleRoot(data:String){
-        let merkleRoot = Blocks()
-        merkleRoot.hash = merkleRoot.generateNewHash()
-        
-        
-        
+    //load data from the database and store it in a string to pass to the block data
+    func fetchData()->String{
+        var userData = String()
+        do {
+            let userBlockData = NSFetchRequest<NSFetchRequestResult>(entityName: "BlockchainData")
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let result = try context.fetch(userBlockData)
+            for data in result as! [NSManagedObject] {
+                userData.append((data.value(forKey: "amount") as! String))
+                userData.append((data.value(forKey: "/") as! String))
+                userData.append((data.value(forKey: "fromusername") as! String))
+                userData.append((data.value(forKey: "/") as! String))
+                userData.append((data.value(forKey: "tousername") as! String))
+                userData.append((data.value(forKey: "/") as! String))
+            }
+        } catch {}
+        return userData
     }
     
+    func createMerkleRoot(){
+        let merkleRoot = Blocks()
+        merkleRoot.hash = merkleRoot.generateNewHash()
+        merkleRoot.userData = fetchData()
+        merkleRoot.prevHash = "nil"
+        merkleRoot.index = 0
+        blockChain.append(merkleRoot)
+    }
     
+    func createBlock(){
+        let newBlock = Blocks()
+        newBlock.hash = newBlock.generateNewHash()
+        newBlock.userData = fetchData()
+        newBlock.prevHash = blockChain[blockChain.count-1].hash
+        newBlock.index = blockChain.count
+        blockChain.append(newBlock)
+    }
     
+
 }
