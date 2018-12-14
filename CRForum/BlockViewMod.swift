f10d5b5506e3c9dc478d89fe68dce0c65c612bda
PZ <40071792+THECH0S3N1@users.noreply.github.com>
2018-12-14 19:01:32 +0100

    v 4.6.1

---
+++ CRForum/BlockViewMod.swift
@@ -0,0 +1,38 @@
+//
+//  BlockViewMod.swift
+//  CRForum
+//
+//  Created by PiotrZ on 10/12/2018.
+//  Copyright © 2018 Z Systems. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import CoreData
+
+class  BlockViewMod: UIViewController {
+    
+    @IBOutlet weak var Label1: UILabel!
+    @IBOutlet weak var Label2: UILabel!
+    @IBOutlet weak var Label3: UILabel!
+    
+    @IBOutlet weak var Label4: UILabel!
+    @IBOutlet weak var Label5: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Label1.text = too
+        Label2.text = fromm
+        Label3.text = String(amountt)
+        Label4.text = prevhashh
+        Label5.text = timestampp
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    
+}
